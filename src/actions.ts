b0c4d05962c7ace57598aed7a14fd78fe197c266
Balte de Wit <balte.dewit@gmail.com>
2020-12-23 09:27:35 +0100

    feat: matrix actions

--- src/actions.ts
+++ src/actions.ts
@@ -1,5 +1,5 @@
 import InstanceSkel = require('../../../instance_skel')
-import { CompanionAction, CompanionActionEvent, CompanionActions, CompanionInputFieldTextInput } from '../../../instance_skel_types'
+import { CompanionAction, CompanionActionEvent, CompanionActions, CompanionInputFieldNumber, CompanionInputFieldTextInput } from '../../../instance_skel_types'
 import { Required as MakeRequired } from 'utility-types'
 import { EmberClient, Model as EmberModel } from 'emberplus-connection'
 import { EmberPlusConfig } from './config'
@@ -8,16 +8,39 @@ export enum ActionId {
   SetValueInt = 'setValueInt',
   SetValueReal = "setValueReal",
   SetValueString = "setValueString",
-  SetValueBoolean = "setValueBoolean"
+  SetValueBoolean = "setValueBoolean",
+  MatrixConnect = "matrixConnect",
+  MatrixDisconnect = "matrixDisconnect",
+  MatrixSetConnection = "matrixSetConnection",
 }
 
 type CompanionActionWithCallback = MakeRequired<CompanionAction, 'callback'>
 
-const pathInput = {
+const pathInput: CompanionInputFieldTextInput = {
   type: 'textinput',
   label: 'Path',
   id: 'path',
 }
+const matrixInputs: Array<CompanionInputFieldTextInput | CompanionInputFieldNumber> = [
+  pathInput,
+  {
+    type: 'number',
+    label: 'Target',
+    id: 'target',
+    required: true,
+    min: 0,
+    max: 0xFFFF,
+    default: 0,
+    step: 1
+  },
+  {
+    type: 'textinput',
+    label: 'Sources',
+    id: 'sources',
+    regex: '/^((\\s*\\d+,)*(\\s*\\d+)$)|$/' // comma separated list
+  },
+]
+
 const setValue = (
   self: InstanceSkel<EmberPlusConfig>,
   emberClient: EmberClient,
@@ -35,7 +58,25 @@ const setValue = (
         self.log('warn', 'Node ' + action.options['path'] + ' is not of type ' + type)
       }
     } else {
-      self.log('warn', 'Node ' + action.options['path'] + ' not found or not a parameter')
+      self.log('warn', 'Parameter ' + action.options['path'] + ' not found or not a parameter')
+    }
+  })
+})
+
+const doMatrixAction = (
+  self: InstanceSkel<EmberPlusConfig>,
+  emberClient: EmberClient,
+  method: EmberClient['matrixConnect'] | EmberClient['matrixDisconnect'] | EmberClient['matrixSetConnection']
+) => ((action: CompanionActionEvent): void => {
+  self.debug('Get node ' + action.options['path'])
+  emberClient.getElementByPath(action.options['path'] as string).then((node) => { // TODO - do we handle not found?
+    if (node && node.contents.type === EmberModel.ElementType.Matrix) {
+      self.debug('Got node on ' + action.options['path'])
+      const target = Number(action.options['target'])
+      const sources = (action.options['sources'] as string).split(',').filter(v => v !== '').map(s => Number(s))
+      method(node as EmberModel.NumberedTreeNode<EmberModel.Matrix>, target, sources)
+    } else {
+      self.log('warn', 'Matrix ' + action.options['path'] + ' not found or not a parameter')
     }
   })
 })
@@ -105,6 +146,27 @@ export function GetActionsList(
       ],
       callback: setValue(self, emberClient, EmberModel.ParameterType.Integer)
     },
+    [ActionId.MatrixConnect]: {
+      label: 'Matrix Connect',
+      options: [
+        ...matrixInputs
+      ],
+      callback: doMatrixAction(self, emberClient, (...args) => emberClient.matrixConnect(...args))
+    },
+    [ActionId.MatrixDisconnect]: {
+      label: 'Matrix Disconnect',
+      options: [
+        ...matrixInputs
+      ],
+      callback: doMatrixAction(self, emberClient, (...args) => emberClient.matrixDisconnect(...args))
+    },
+    [ActionId.MatrixSetConnection]: {
+      label: 'Matrix Set Connection',
+      options: [
+        ...matrixInputs
+      ],
+      callback: doMatrixAction(self, emberClient, (...args) => emberClient.matrixSetConnection(...args))
+    }
   }
 
   return actions
